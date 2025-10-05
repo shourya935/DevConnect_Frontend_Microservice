9701887a2c06115c37b13f51672398f5e804d8db
shourya93e <shouryajain333@gmail.com>
2025-10-05 18:44:47 +0530

    Few Changes During Deployment

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-const BASE_URL = import.meta.env.VITE_BASE_URL
+import React, { useEffect, useState } from "react";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 import axios from "axios";
 import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import NavBar from "./Navbar";
@@ -10,31 +10,47 @@ import { addUser } from "../ustils/userSlice";
 function Body() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const location = useLocation(); 
+  const location = useLocation();
   const userData = useSelector((store) => store.user);
+  const [loading, setLoading] = useState(true);
 
-  const fetchUser = async () => {
-    try {
-      const res = await axios.get(BASE_URL + "/profile/veiw", {
-        withCredentials: true,
-      });
-      dispatch(addUser(res.data));
-    } catch (err) {
-      if (err?.response?.status === 401) {
-        navigate("/login");
-      }
-      console.error(err);
-    }
-  };
+  const isLoggedIn = !!userData?._id; // false or true 
+  
 
   useEffect(() => {
-    if (!userData) {
-      fetchUser();
-    }
-  }, []);
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(BASE_URL + "/profile/veiw", {
+          withCredentials: true,
+        });
+        dispatch(addUser(res.data));
+      } catch (err) {
+        if (err?.response?.status === 401) {
+          navigate("/login");
+        }
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+  }, [dispatch, navigate]);
 
   const hideNavRoutes = ["/login", "/signup"];
   const shouldHideNav = hideNavRoutes.includes(location.pathname);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!loading && !isLoggedIn) {
+    navigate("/login");
+    return null;
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
